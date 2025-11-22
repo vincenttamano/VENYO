d012cb321d3ca8c2402f97c832ad5f47773472ef
vincenttamano <vincentjohn.tamano@wvsu.edu.ph>
2025-11-22 16:32:29 +0800

    Delete backendTrial directory

--- backendTrial/backendTrial/src/main/java/Venyo/ManageVenue.java
+++
@@ -1,7 +0,0 @@
-package Venyo;
-
-public interface ManageVenue {
-    public void addVenue(Venue venue);
-    public void removeVenue(Venue venue);
-    public void updateVenue(Venue venue);
-}
--- backendTrial/backendTrial/src/main/java/Venyo/Payment.java
+++
@@ -1,5 +0,0 @@
-package Venyo;
-
-public interface Payment {
-    public void paymentMethod(double amount);
-}
